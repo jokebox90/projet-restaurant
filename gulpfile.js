91914a0481806eb51202b8de23455d1628e48ad5
Jonathan Vagnier <adm.jvagnier@gmail.com>
2022-01-13 20:49:50 +0100

    Fixe la configuration de gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const nodemon = require('gulp-nodemon')
 const browserSync = require('browser-sync').create()
 const sass = require('gulp-sass')(require('sass'))
 
-function server(done) {
+function serverTask(done) {
   nodemon({
-    script: '.', //this is where my express server is
+    script: 'server.js', //this is where my express server is
     ext: 'js html css', //nodemon watches *.js, *.html and *.css files
     env: { 'NODE_ENV': 'development' }
   })
@@ -13,31 +13,34 @@ function server(done) {
   done()
 }
 
-exports.server = server
+exports.serverTask = serverTask
 
-function watcher(done) {
+function browserSyncTask(done) {
   browserSync.init({
     port: 8887, //this can be any port, it will show our app
     proxy: 'http://localhost:8888/', //this is the port where express server works
     ui: { port: 8886 }, //UI, can be any port
     reloadDelay: 1000 //Important, otherwise syncing will not work
   })
+}
 
-  gulp.watch(['./**/*.js', './**/*.html', './**/*.css']).on("change", browserSync.reload)
+exports.browserSyncTask = browserSyncTask
+
+function sassTask(done) {
+  gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('assets/css/'))
 
   done()
 }
 
-exports.watcher = watcher
+exports.sassTask = sassTask
 
-function builder(done) {
-    gulp.src('scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
-
-    done()
+function sassWatch() {
+  gulp.watch('scss/**/*.scss', gulp.series(sassTask, browserSync.reload))
+  gulp.watch(['scss/**/*.scss']).on("change", gulp.series(sassTask, browserSync.reload))
 }
 
-exports.builder = builder
+exports.sassWatch = sassWatch
 
-exports.default = gulp.series(builder, watcher, server)
+exports.default = gulp.parallel(sassWatch, serverTask, browserSyncTask)
